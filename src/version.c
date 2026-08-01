09e2d19763613d6d9f046d6596bf4db0ad430151
Edward Bennett <44945949+ebenne01@users.noreply.github.com>
2026-08-01 00:30:41 -0400

    Example 00-00 ncurses version

---
+++ src/version.c
@@ -0,0 +1,11 @@
+#include <ncurses.h>
+#include <stdio.h>
+
+int main() {
+  printf("Ncurses version %d.%d.%d\n",
+    NCURSES_VERSION_MAJOR,
+    NCURSES_VERSION_MINOR,
+    NCURSES_VERSION_PATCH
+  );
+  return 0;
+}
