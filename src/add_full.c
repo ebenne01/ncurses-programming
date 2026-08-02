885fd125e21754723a9143539c5911e2c6e966e4
Edward Bennett <44945949+ebenne01@users.noreply.github.com>
2026-08-02 14:34:58 -0400

    Example 02-03 add full string

---
+++ src/add_full.c
@@ -0,0 +1,29 @@
+/*
+ * add.c
+ *
+ * Demonstrates displaying full string of text
+ * using ncurses.
+ *
+ * Based on:
+ *   Dan Gookin's Guide to Ncurses Programming
+ *   Chapter 2, Exercise 3
+ */
+
+#include <ncurses.h>
+
+int main(void) {
+  char t1[] = "Shall I compare thee";
+  char t2[] = " to a summer's day?";
+
+  initscr();
+
+  addstr(t1);
+  addstr(t2);
+
+  refresh();
+  getch();
+
+  endwin();
+
+  return 0;
+}
